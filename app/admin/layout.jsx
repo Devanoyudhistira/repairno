2b43bc251125bf27288b6a3145491fa490d594cf
Devanoyudhistira <devanotira@gmail.com>
2026-06-26 08:37:05 +0700

    fix:sidebar position on mobile

--- app/admin/layout.jsx
+++ app/admin/layout.jsx
@@ -14,10 +14,11 @@ export default async function Layout({ children }) {
 
     return (
         <>
-            <div className="w-screen flex items-center gap-3" >
+            <div className="w-screen lg:flex items-center gap-3" >
                 <Sidebarnav />
-                <div className="col-span-2 w-[90%]" >
-                {children}
+                <div className="col-span-2 w-full px-2 lg:px-0 lg:w-[90%]" >
+                    <SidebarTrigger />
+                    {children}
                 </div>
             </div>
         </>
