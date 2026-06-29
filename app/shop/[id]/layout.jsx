f3108d256e8f1f7ea7cce9e50eec0bc6ae583b59
Devanoyudhistira <devanotira@gmail.com>
2026-06-29 22:29:32 +0700

    fix:add price on title

--- app/shop/[id]/layout.jsx
+++ app/shop/[id]/layout.jsx
@@ -1,3 +1,4 @@
+import convertToMoney from "@/function/convert";
 import imageurl from "@/lib/imageurl";
 import truncate from "@/lib/truncat";
 import supabase from "@/supabase/supabase";
@@ -6,13 +7,13 @@ import supabase from "@/supabase/supabase";
 export async function generateMetadata({ params }) {
     const { id } = await params;
 
-    const { data: shop, error } = await supabase.from("shop").select("description,name,gambar").eq("id", id).single()    
+    const { data: shop, error } = await supabase.from("shop").select("description,name,gambar,price").eq("id", id).single()    
     return {
         title: shop.name,
         description: truncate(shop.description, 15),
         openGraph: {
             title: shop.name,
-            description: truncate(shop.description, 15),
+            description: truncate(shop.description, 25) + " " + convertToMoney( shop.price ),
             images: [
                 {
                     url: imageurl(shop.gambar),
@@ -25,7 +26,7 @@ export async function generateMetadata({ params }) {
 
         twitter: {
             card: "summary_large_image",
-            title: shop.name,
+            title: shop.name + shop.price,
             description: shop.description,
             images: [imageurl(shop.gambar)],
         },
