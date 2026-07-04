643a0871ceb22532a7d27160dd1ddfba5a8e9ec8
Devanoyudhistira <devanotira@gmail.com>
2026-07-04 10:28:24 +0700

    fix:spec now read from real data

--- app/shop/[id]/page.jsx
+++ app/shop/[id]/page.jsx
@@ -4,6 +4,7 @@ import Navbar from "@/components/navbar"
 import Slideitem from "@/components/slideitem"
 import { Button } from "@/components/ui/button"
 import { Card, CardAction, CardContent, CardDescription, CardHeader } from "@/components/ui/card"
+import { Separator } from "@/components/ui/separator"
 import { Toaster } from "@/components/ui/sonner"
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
 import convertToMoney from "@/function/convert"
@@ -14,29 +15,35 @@ import Image from "next/image"
 export default async function Page({ params }) {
     const { id } = await params
     const { data, error } = await supabase.from("shop").select("*").eq("id", id).single()
+    const { data: specdata, error: specerror } = await supabase.from("spec").select("*").eq("item", id)
+    console.log(id)
+    console.log(specdata)
+    console.log(specerror)
     return (
         <div>
             <Toaster />
             <Navbar singlepage={true} addcontext={"Product detail"} />
             <main className={`w-full flex flex-col lg:px-4 lg:flex-row  items-center`} >
                 <Slideitem image={imageurl(data.gambar)} />
-                <Card className={`p-2 lg:ml-0 mr-3 h-130 lg:-mt-10 lg:h-100 justify-between bg-transparent border-transparent border-0  border-none mt-2 w-[92%] lg:w-[60%] `} >
+                <Card className={`p-2 lg:ml-0 mr-3 min-h-130 max-h-max lg:-mt-10 lg:h-100 justify-between bg-transparent border-transparent border-0  border-none mt-2 w-[92%] lg:w-[60%] `} >
                     <div className="flex flex-col gap-4 " >
                         <CardHeader className={`w-full font-semibold border-transparent border-none border-0 text-xl p-0 flex justify-between`} >
                             <h1> {data.name} </h1>
-                            <h1> {convertToMoney(data.price)} </h1>
                         </CardHeader>
+                        <CardDescription className={`text-primary font-medium text-[17px]`} >
+                            {data.description} Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolores illum adipisci officia! Facilis nam similique recusandae odio possimus asperiores quos.
+                        </CardDescription>
+                        <Separator />
+                        <h1 className="text-2xl ml-4 font-bold" > {convertToMoney(data.price)} </h1>
+                        <Separator />
                         <CardContent className={`p-0 w-full`} >
-                            <h1 className="text-primary text-xl font-semibold capitalize" > Detail product </h1>
-                            <ul className="w-full px-2 text-md flex flex-col gap-3 mt-2 " >
-                                <Detatiledlist value={"baru"} category={"Condition"} />
-                                <Detatiledlist category={"spesifikasi"} value={"8 gigabyte"} />
-                                <Detatiledlist category={"garansi"} value={"2 tahun"} />
+                            <h1 className="text-primary text-2xl font-semibold capitalize" > spesifikasi product </h1>
+                            <ul className="w-full px-2 text-md flex flex-col gap-3 mt-8 " >
+                                {specdata.map(e =>
+                                    <Detatiledlist value={e.value} category={e.name}  key={e.id} />
+                                )}
                             </ul>
                         </CardContent>
-                        <CardDescription className={`text-primary font-medium text-md`} >
-                            {data.description}
-                        </CardDescription>
                     </div>
                     <CardAction className={`p-0 w-full`} >
                         <Buyitem id={data.id} />
--- components/detailedlist.jsx
+++ components/detailedlist.jsx
@@ -1,10 +1,13 @@
 "use client"
 
+import { Separator } from "./ui/separator"
+
 export default function Detatiledlist({value,category}) {
     return (
-        <li className="w-full justify-between flex gap-3 items-center " >
-            <h2 className="text-secondary-foreground font-medium capitalize " > {category} </h2>
-            <h2 className="text-secondary-foreground font-medium capitalize " > {value} </h2>
+        <li className="w-full grid-cols-2 items-end content-end justify-end grid  gap-3  " >
+            <h2 className="text-secondary-foreground text-xl uppercase font-medium flex items-center justify-start" > {category} </h2>
+            <h2 className="text-secondary-foreground text-xl w-max font-normal capitalize  flex items-center justify-end" > {value} </h2>
+            <Separator className={"col-span-2"} />
         </li>
     )
 }
