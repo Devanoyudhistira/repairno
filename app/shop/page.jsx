8f4cfe213858bad7c72119ff8cacb5aeeb24481a
Devanoyudhistira <devanotira@gmail.com>
2026-07-12 20:52:19 +0700

    add:empty screen

--- app/shop/page.jsx
+++ app/shop/page.jsx
@@ -6,13 +6,16 @@ import Productcard from "@/components/productcard";
 import Searchfunction from "@/components/searchfunction";
 import { DropdownMenu } from "@/components/ui/dropdown-menu";
 import supabase from "@/supabase/supabase";
+import { Search } from "lucide-react";
 import { Suspense } from "react";
 export const dynamic = "force-dynamic";
 
 export default async function Page({ searchParams }) {
     const { search, category, urutkan } = await searchParams
     let alldata = supabase.from("shop").select("*", { count: "exact" })
-    if (search && search.length >= 1) {
+    const searchquery = search && search.length >= 1
+    const activesearch = searchquery
+    if (searchquery) {
         alldata = alldata.or(`name.ilike.%${search}%`)
     }
     if (category) {
@@ -43,8 +46,7 @@ export default async function Page({ searchParams }) {
 
 
     const { data, error } = await alldata
-    console.log(data)
-    console.log(error)
+    console.log(data.length)
     return <main className="text-primary" >
         <Navbar />
         <div className="w-full lg:px-10 px-3 mt-4" >
@@ -56,6 +58,12 @@ export default async function Page({ searchParams }) {
                 <Clearfilter condition={category || search || urutkan} />
             </div>
         </div>
+        {activesearch && data.length === 0 &&
+            <div className="w-full flex flex-col items-center gap-2 justify-center" >
+                <h1 className="mt-10 text-3xl lg:text-6xl font-medium" > barang tidak ditemukan </h1>
+                <h2 className="text-xl lg:text-5xl gap-3 text-foreground flex items-center font-thin" > cari lagi <Search className="size-8 font-thin" /> </h2>
+            </div>
+        }
         <article className="w-full px-3 grid gap-x-4 gap-y-3 mt-2 grid-cols-2 lg:grid-cols-4 md:grid-cols-3" >
             {data.map(e =>
                 <Productcard stock={e.stock} id={e.id} key={e.id} productname={e.name} image={e.gambar} harga={e.price} />
