b42a7106314cc256a127c38576cc5418747a7b5b
Devanoyudhistira <devanotira@gmail.com>
2026-07-19 10:27:31 +0700

    fix:navbar build

--- components/navbar.jsx
+++ components/navbar.jsx
@@ -5,12 +5,17 @@ import { ChevronLeft } from "lucide-react"
 import Link from "next/link"
 import { createClient } from "@/supabase/server"
 import Navadmin from "./admin/navadmin"
+import { useradmin, userimage } from "@/lib/userid"
+import Image from "next/image"
+import imageurl from "@/lib/imageurl"
 
 
 export default async function Navbar({ singlepage, addcontext }) {
     const supabaseauth = await createClient()
     const isadmin = await supabaseauth.auth.getSession()
     const admin = isadmin.data.session
+    const realadmin = await useradmin()
+    const profileimage = await userimage()
     return (<nav className="w-full h-14 border-secondary border-b-2 flex items-center justify-between px-3" >
         <h1 className="text-primary font-semibold text-xl" > {singlepage ?
             <span className="flex items-center gap-2" >
@@ -19,9 +24,9 @@ export default async function Navbar({ singlepage, addcontext }) {
                 </Link>  {addcontext} </span>
             :
             "devacom"}  </h1>
-        <div>
+        { !realadmin ? <Image src={imageurl(`/profile/${profileimage}`)} className="w-10 h-10 mr-3 rounded-full object-center object-cover" alt={profileimage} width={100} height={100} />  : <div>
            {!admin && <Link href={"/sign-up"} className="text-2xl font-medium text-secondary" > Login </Link>}
             {admin ? <Navadmin /> : <Modetoggle />}
-        </div>
+        </div>}
     </nav>)
 }
