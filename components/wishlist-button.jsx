f4d8598ea997145c2c7f1989fcd64498e89c801d
Devanoyudhistira <devanotira@gmail.com>
2026-07-21 11:02:09 +0700

    fix:make a debounce function for wishlist

--- components/wishlist-button.jsx
+++ components/wishlist-button.jsx
@@ -1,7 +1,55 @@
 "use client"
 
 import { Heart } from "lucide-react"
+import { useRouter } from "next/navigation"
+import { useState } from "react"
+import { toast } from "sonner"
 
-export default function Wishlistbutton({handle,wishlisht}){
-    return <button onClick={handle} className="absolute top-2 left-3" > <Heart className={`text-red-700 size-8 transition ${wishlisht && 'fill-red-700'} `} /> </button> 
+export default function Wishlistbutton({ handle, wishlisht ,id,setiswishlist}) {
+    const [isloading, setloading] = useState(false)
+    const router = useRouter()
+    console.log("Dadad")
+    console.log(isloading)
+    const bodydata = JSON.stringify({ id: id })
+    async function addwishlist() { 
+        setloading(true)       
+        if (!wishlisht) {
+            const wishrequest = await fetch(`${process.env.NEXT_PUBLIC_URL}/api/wishlist`, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: bodydata
+            })
+            const response = await wishrequest.json()
+            if (response.success) {                
+                setiswishlist(true)
+                router.refresh()
+                setloading(false)
+                return response
+            }
+            setloading(false)
+        }
+        else {
+            const wishrequest = await fetch(`${process.env.NEXT_PUBLIC_URL}/api/wishlist`, {
+                method: "DELETE",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body:bodydata,
+            })
+            const response = await wishrequest.json()
+            if (response.success) {            
+                router.refresh()
+                setiswishlist(false)
+                setloading(false)
+                return response
+            }
+            setloading(false)
+        }
+    }
+    return <button disabled={isloading} onClick={() => toast.promise(addwishlist(), { position: "top-center", loading: "loading..", success: (response) => response.message, error: "gagal silahkan coba lagi" })}
+        className="absolute top-2 left-3" >
+        <Heart className={`text-red-700 size-8 transition ${wishlisht && 'fill-red-700'} `} />
+    </button>
 }
